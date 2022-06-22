936d2cf09abd7cb99ce5032aff6f939d15e2d8ad
Sergio Tkaczek <sergioatk@gmail.com>
2022-06-22 18:44:00 -0300

    update get res

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>Express con HTML!</h1>");
 });
 app.listen(PORT, console.log(`listening on port ${PORT}`));
