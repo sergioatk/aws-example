a5232e12dce8a187d774bb4ade9430a19ce48ab9
Sergio Tkaczek <sergioatk@gmail.com>
2022-06-22 18:20:55 -0300

    my initial server

---
+++ server.js
@@ -0,0 +1,9 @@
+const PORT = 3000;
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, console.log(`listening on port ${PORT}`));
